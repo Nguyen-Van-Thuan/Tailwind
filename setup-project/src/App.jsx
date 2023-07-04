5d2bb94f5e9f5d9807ebaae71b269321e8344e3c
thuan.nv <nguyenthuankma@gmail.com>
2023-07-04 16:42:48 +0700

    update souce code tailwind

---
+++ setup-project/src/App.jsx
@@ -0,0 +1,62 @@
+function App() {
+  return (
+    <>
+      <h1 className="pt-12 text-center font-semibold text-black text-4xl ">
+        Live anywhere
+      </h1>
+      <h2 className="text-center font-light text-xl text-gray-500 mb-35">
+        Keep calm & travel on
+      </h2>
+
+      <div className="grid grid-cols-3 gap-x-8 max-w-6xl mx-auto">
+        <div>
+          <div className="h-96 mb-5">
+            <img
+              src="https://images.unsplash.com/photo-1688295598146-028a57f4c939?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=857&q=80"
+              alt=""
+              className="w-full h-full rounded-lg object-cover"
+            />
+          </div>
+          <h3 className="text-center font-medium text-lg mb-3">
+            Enjoy the great cold
+          </h3>
+          <span className="block text-center  text-gray-400 text-sm">
+            6.789 properties
+          </span>
+        </div>
+        <div>
+          <div className="h-96 mb-5">
+            <img
+              src="https://images.unsplash.com/photo-1688295598146-028a57f4c939?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=857&q=80"
+              alt=""
+              className="w-full h-full rounded-lg object-cover"
+            />
+          </div>
+          <h3 className="text-center font-medium text-lg mb-3">
+            Enjoy the great cold
+          </h3>
+          <span className="block text-center  text-gray-400 text-sm">
+            6.789 properties
+          </span>
+        </div>
+        <div>
+          <div className="h-96 mb-5">
+            <img
+              src="https://images.unsplash.com/photo-1688295598146-028a57f4c939?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=857&q=80"
+              alt=""
+              className="w-full h-full rounded-lg object-cover"
+            />
+          </div>
+          <h3 className="text-center font-medium text-lg mb-3">
+            Enjoy the great cold
+          </h3>
+          <span className="block text-center  text-gray-400 text-sm">
+            6.789 properties
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
